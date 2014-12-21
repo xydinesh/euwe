c700d236fa4313fb130e4d03a8685e7a1ae1dfa6
Dinesh Weerapurage <xydinesh@gmail.com>
2014-12-21 16:20:21 -0500

    change static file view to serve img and and js inside static directory

--- euwe/__init__.py
+++ euwe/__init__.py
@@ -15,7 +15,9 @@ def main(global_config, **settings):
     Base.metadata.bind = engine
     config = Configurator(settings=settings)
     config.include('pyramid_mako')
-    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_static_view('static', 'static', cache_max_age=600)
+    config.add_static_view('img', 'static/img', cache_max_age=60)
+    config.add_static_view('js', 'static/js', cache_max_age=600)
     config.add_route('home', '/')
     config.scan()
     return config.make_wsgi_app()
