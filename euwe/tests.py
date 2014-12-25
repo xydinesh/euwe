f4582eb3802b0ddea669f7a088ce5d1d361ae55e
Dinesh Weerapurage <xydinesh@gmail.com>
2014-12-25 11:20:50 -0500

    aded more unit tests for testing login and logout

--- euwe/tests.py
+++ euwe/tests.py
@@ -6,23 +6,31 @@ import transaction
 from pyramid import testing
 from .models import DBSession
 
+def _initDB():
+    from sqlalchemy import create_engine
+    engine = create_engine('sqlite://')
+    from .models import (
+        Base,
+        MyModel,
+        PositionModel,
+        )
+    DBSession.configure(bind=engine)
+    Base.metadata.create_all(engine)
+    with transaction.manager:
+        model = MyModel(name='one', value=55)
+        DBSession.add(model)
+
+        position = PositionModel(category='position', fen='r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R')
+        DBSession.add(position)
+    return DBSession
+
 class TestMyViewSuccessCondition(unittest.TestCase):
     def setUp(self):
         self.config = testing.setUp()
-        from sqlalchemy import create_engine
-        engine = create_engine('sqlite://')
-        from .models import (
-            Base,
-            MyModel,
-            )
-        DBSession.configure(bind=engine)
-        Base.metadata.create_all(engine)
-        with transaction.manager:
-            model = MyModel(name='one', value=55)
-            DBSession.add(model)
+        self.session = _initDB()
 
     def tearDown(self):
-        DBSession.remove()
+        self.session.remove()
         testing.tearDown()
 
     def test_passing_view(self):
@@ -33,23 +41,56 @@ class TestMyViewSuccessCondition(unittest.TestCase):
         self.assertEqual(info['one'].name, 'one')
         self.assertEqual(info['project'], 'euwe')
 
+
+
 class EuweUnitTestViews(unittest.TestCase):
     def setUp(self):
         self.config = testing.setUp()
+        self.session = _initDB()
+
+    def tearDown(self):
+        self.session.remove()
+        testing.tearDown()
+
+    def test_login_view(self):
         # this tests throw an exception
         # found answer here
         # https://github.com/Pylons/pyramid/issues/1202
         self.config.add_route('login', '/login')
-
-    def tearDown(self):
-        testing.tearDown()
-
-    def test_login_view(self):
         from .views import EuweViews
         request = testing.DummyRequest()
         inst = EuweViews(request)
         info = inst.login_view()
         self.assertEqual('Euwe Login Page', info['title'])
+        self.assertIn('Invalid Login, Try again', info['message'])
+
+    def test_login_success_view(self):
+        # this tests throw an exception
+        # found answer here
+        # https://github.com/Pylons/pyramid/issues/1202
+        self.config.add_route('login', '/login')
+        from .views import EuweViews
+        request = testing.DummyRequest(params={'username': 'max', 'password': 'user_max', 'form.submitted': True})
+        inst = EuweViews(request)
+        info = inst.login_view()
+        self.assertEqual(info.status_int, 302)
+
+    def test_logout_view(self):
+        # this tests throw an exception
+        # found answer here
+        # https://github.com/Pylons/pyramid/issues/1202
+        self.config.add_route('home', '/')
+        self.config.add_route('logout', '/logout')
+        from .views import EuweViews
+        request = testing.DummyRequest()
+        inst = EuweViews(request)
+        info = inst.logout_view()
+        self.assertEqual(info.status_int, 302)
+
+    def test_position_model(self):
+        from .models import PositionModel
+        self.assertTrue(True)
+
 
 class EuweFunctionalTests(unittest.TestCase):
     def setUp(self):
@@ -87,6 +128,10 @@ class EuweFunctionalTests(unittest.TestCase):
         form['password'] = 'max123'
         res = form.submit('form.submitted')
         self.assertIn(b'Invalid Login, Try again', res.body)
+
+    def test_logout_url(self):
+        res = self.testapp.get('/logout', status=302)
+        self.assertEqual(res.status_int, 302)
 
 
 
