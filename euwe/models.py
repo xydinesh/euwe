337580340808cf71d71a315eb415ca5eeb48bc2b
Dinesh Weerapurage <xydinesh@gmail.com>
2014-12-25 11:57:31 -0500

    added new model for saving positions

--- euwe/models.py
+++ euwe/models.py
@@ -1,7 +1,11 @@
+from datetime import datetime
+
 from sqlalchemy import (
     Column,
     Index,
     Integer,
+    String,
+    DateTime,
     Text,
     )
 
@@ -36,3 +40,38 @@ class MyModel(Base):
     value = Column(Integer)
 
 Index('my_index', MyModel.name, unique=True, mysql_length=255)
+
+class PositionModel(Base):
+    __tablename__ = 'tbl_positions'
+    id = Column(Integer, primary_key=True)
+    fen = Column(String(128))
+    category = Column(Integer)
+    hint = Column(String(128))
+    instructions = Column(String(1024))
+    solution = Column(Text)
+    move = Column(Integer)
+    mate_in = Column(Integer)
+    pgn = Column(Text)
+    created = Column(DateTime)
+    modified = Column(DateTime)
+
+    categories = {
+        'game': 1,
+        'problem': 2,
+        'position': 3,
+        'analysis': 4,
+    }
+
+    def __init__(self, category, fen=None, pgn=None):
+        self.category_key = category
+        if fen:
+            self.fen = fen
+        if pgn:
+            self.pgn = pgn
+            
+        self.category = self.categories[category]
+        self.created = datetime.utcnow()
+        self.modified = datetime.utcnow()
+
+    def __repr__(self):
+        return b'Position <{0} {1}>'.format()
